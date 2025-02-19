8ecae0bf959e0aba9e70a43d1b43a838db0e3cbb
Mylena <mylena.silva@brpartners.com.br>
2025-02-19 14:34:49 -0300

    feat: add cors dependency for handling cross-origin requests

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express"); // Import express
+const cors = require("cors") 
 const app = express(); // Create an express app
 const bookRouter = require("./routes/book"); // Import the bookRouter
 
 const port = 8000 // Port number
 
 app.use(express.json()); // Middleware to parse JSON bodies
+app.use(cors({origin: '*'})); // Middleware to allow requests from any origin
+
 app.use("/books", bookRouter); // Use the bookRouter for all routes that start with /books
 
 // app.get("/", (req, res) => {
